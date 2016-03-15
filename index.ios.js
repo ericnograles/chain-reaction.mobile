fba78e8bfd61f21164d2cfb72cebec6e7923361c
Eric Nograles <ENograles@352inc.com>
2016-03-15 16:25:34 -0400

    Moving all constituent components to their respective GH repos

---
+++ index.ios.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { Provider } from 'react-redux';
+import App from './src/containers/App';
+import configureStore from '../common/store/configureStore';
+
+const store = configureStore();
+
+class chainreaction extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App store={store} />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('chainreaction', () => chainreaction);
