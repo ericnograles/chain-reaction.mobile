7618f1c57812e347568f61a8997a9d91d37019e4
Eric Nograles <ENograles@352inc.com>
2016-04-25 10:37:29 -0400

    Upgrading to RN 0.24.  0.22 was causing issues

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 # Assumptions: NVM installed
 # Add a reference to nvm
-# export NVM_DIR=~/.nvm
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR=~/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 # Baseline to Node LTS
-# nvm install 4.3.1
+nvm install 4.3.1
 
 # Global prerequisites
-# npm install -g npm3
+npm install -g npm3
 
 # Globals for npm3
-# npm3 install -g react-native-cli
+npm3 install -g react-native-cli
 
 # Local NPM install
-# npm3 install
+npm3 install
 
 # Launch iOS, Packager, and Nodemon to watch for changes in chain-reaction.common
 trap 'kill %1; kill %2' SIGINT
